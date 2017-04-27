4e196f585913d4c3b29f8a8cbc3fc6f7b3631e8d
Mark Miller <marklmiller2010@gmail.com>
2017-04-26 20:27:21 -0400

    Add files via upload

---
+++ Symbol.cpp
@@ -0,0 +1,46 @@
+ #include "Symbol.h"
+ #include<string>
+
+ Symbol::Symbol(std::string Label, int address, int length)
+ {
+  Symbol::label = Label;
+  Symbol::Address = address;
+  Symbol::Length = length;
+ }
+ Symbol::~Symbol()
+ {
+     this.delete;
+ }
+
+ std::string Symbol::getLabel()
+ {
+   return Symbol:label;
+ }
+
+ void Symbol::setLabel(std::string Label)
+ {
+  Symbol::label = Label;
+ }
+
+ int Symbol::getAddress()
+ {
+  return Symbol::Address;
+ }
+
+ void Symbol::setAddress(int address)
+ {
+  Symbol::Address = address;
+ }
+
+ int Symbol::getLength()
+ {
+  return Symbol::Length;
+ }
+ void Symbol::setLength(int length)
+ {
+  Symbol::Length = length;
+ }
+
+
+
+
---
+++ Symbol.h
@@ -0,0 +1,31 @@
+ #ifndef SYMBOL_H
+ #define SYMBOL_H
+
+ #include<string>
+ #include<vector>
+
+ class Symbol {
+  private:
+  //Class Variables
+    std::string label; //label is just the label name for label portion of the source code
+    int Address, Length; //Address is the location of the symbol being stored.  Length is the length of the symbol in bytes
+
+  public:
+
+
+  //Constructor
+    Symbol(std::string Label, int address, int length);
+
+  //Destructor
+    ~Symbol();
+
+  //Class Methods
+    std::string getLabel();
+    void setLabel(std::string symbol);
+    int getAddress();
+    void setAddress(int address);
+    int getLength();
+    void setLength(int length);
+
+	}
+ #endif
