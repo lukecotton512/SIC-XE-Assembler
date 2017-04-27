27e040cf7db76acd679573f507e239a7011cc216
Mark Miller <marklmiller2010@gmail.com>
2017-04-26 21:36:26 -0400

    Added SYMTABLE.h and SYMTABLE.cpp

---
+++ SYMTABLE.cpp
@@ -0,0 +1,27 @@
+ #include "SYMTABLE.h"
+ #include "Symbol.h"
+
+ SYMTABLE::SYMTABLE()
+ {
+  //SYMBOL TABLE STAYS EMPTY UNTIL SOURCE FILE SYMBOLS ARE ADDED TO IT
+ }
+ void SYMTABLE::addSymbol(std::string label, int Address, int length)
+ {
+    Symbol newSymbol;                //creates new symbol and assigns the symbol value.
+    newSymbol.setLabel(label);
+    newSymbol.setAddress(Address);
+    newSymbol.setLength(length);
+    SYMTABLE::SymbolTable.push_back(newSymbol);//new symbol is inserted into the vector
+ }
+
+ Symbol::Symbol getSymbol(String label)
+ {
+     for(int i = 0; i < SYMTABLE::SymbolTable.Size(); i++)
+         {
+          if(SYMTABLE::SymbolTable.at(i).getLabel() == label)
+             {
+               return SYMTABLE::SymbolTable.at(i);  //Searches the SYMTABLE for a matching label and if found it returns that symbol
+             }
+         }
+      return null;   //If no matching symbol is found in the SYMTABLE then a null value is returned
+ }
---
+++ SYMTABLE.h
@@ -0,0 +1,17 @@
+ #ifndef SYMTABLE_H
+ #define SYMTABEL_H
+
+ #include "Symbol.h"
+ #include <vector>
+ #include <string>
+
+ class SYMTABLE {
+  public:
+      vector<Symbol> SymbolTable; //will contain a list of all symbols
+
+      SYMTABLE();
+
+      void addSymbol(std::string label, int Address, int length); //allows you to insert a symbol into the vector
+
+      Symbol::Symbol getSymbol(String label); //allows you to retrieve a Symbol from the symbol table
+ }
